f2ad1f92f0fcf481752e2f24f24c57e90b987005
Chanv Kuldeep <93032744+chanvkuljit@users.noreply.github.com>
2023-02-02 20:57:56 +0530

    Create 5.py

---
+++ python/5.py
@@ -0,0 +1,9 @@
+'''
+5.Write a program to input radius of a circle, and print area of 
+that circle. 
+'''
+PI=3.14
+radius=float(input("Enter radius of circle:"))
+
+area=PI*pow(radius,2)
+print("Area of Circle is :",+area)
