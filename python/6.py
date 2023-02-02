62e6df22754015dda9ca5055aa6f4df936e50f23
Chanv Kuldeep <93032744+chanvkuljit@users.noreply.github.com>
2023-02-02 21:00:49 +0530

    Create 6.py

---
+++ python/6.py
@@ -0,0 +1,10 @@
+'''
+6.Write a program to input a number and print whether it is Even 
+or Odd Number. 
+'''
+num=int(input("Enter Number:"))
+
+if(num%2)==0:
+    print("Number is Even")
+else :
+    print("Number is Odd")
