f619ab183175ca0ec11236d0a38fb01880b4d7fa
Chanv Kuldeep <93032744+chanvkuljit@users.noreply.github.com>
2023-02-02 20:27:14 +0530

    Create 1.py

---
+++ python/1.py
@@ -0,0 +1,19 @@
+'''
+1.Write a simple Python Program to INPUT two variables and print 
+Addition, Subtraction, Multiplication and Division of both 
+numbers.
+'''
+
+num1=int(input("1 Number :"))
+num2=int(input("2 Number :"))
+
+Addition=num1+num2
+Subtraction=num1-num2
+Division=num1/num2
+Multiplication=num1*num2
+
+print("{} is Addition of two number".format(Addition))
+print("{} is Subtraction of two number".format(Subtraction))
+print("{} is Division of two number".format(Multiplication))
+print("{} is Multiplication of two number".format(Division))
+
