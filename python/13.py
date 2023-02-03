b00f7de1e7282d135d0bb7c8c494cdb24248f708
Chanv Kuldeep <93032744+chanvkuljit@users.noreply.github.com>
2023-02-03 14:26:46 +0530

    Create 13.py

---
+++ python/13.py
@@ -0,0 +1,11 @@
+'''
+13.Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5, between 2000 and 3200 (both included).
+'''
+num=[]
+
+for i in range(2000,3200):
+    if((i%7==0) and (i%5!=0)):
+        num.append(str(i))
+        print (',',+num)
+      
+    
